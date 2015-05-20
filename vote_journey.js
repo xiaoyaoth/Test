ecf3b74081661ba17a823a58034460730fae3d77
xiaoyaoth <xli15@e.ntu.edu.sg>
2015-05-20 15:14:46 +0800

    haha

--- vote_journey.js
+++ vote_journey.js
@@ -7,7 +7,7 @@
 // @match         http://www.enet.com.cn/enews/zhuanti/2015/vote_journey/?from=singlemessage&isappinstalled=0
 // ==/UserScript==
 
-unsafeWindow.alert = function alert(message) {console.log(message)};
+//unsafeWindow.alert = function alert(message) {console.log(message)};
 tpv(268,1);
 var waitTime = 30000 + Math.round(Math.random() * 1000);
 console.log(waitTime);
